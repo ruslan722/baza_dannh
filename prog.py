6db6897bb44a3d8a5b094660110f5fc53e631218
John Doe <johndoe@email.com>
2025-08-28 14:41:05 +0300

    ФИКС 2

--- prog.py
+++ prog.py
@@ -9,11 +9,19 @@ root.geometry('800x600')
 root.resizable(False, False)
 root.configure(bg='#FFFFFF')
 
-def first_window(event=None):
-    '''Функционал окна '''
 
-    shapla = Label(root, text='Первое окно', bg='#2D6033', fg ='white',
-                font=('Gabriolla', 20), width=50, height=2)
+def clear_window():
+    for widget in root.winfo_children():
+        if widget not in (button_back, button_forward):
+            widget.destroy()
+
+
+def first_window(event=None):
+    clear_window()
+    '''Функционал окна '''
+    shapla = Label(root, text='Первое окно', bg='#2D6033',
+                   fg='white',
+                   font=('Gabriolla', 20), width=50, height=2)
     shapla.place(x=0, y=15)
 
     image = PhotoImage(file='icon.png')
@@ -31,26 +39,31 @@ def first_window(event=None):
     y_posihion = 100
 
     for i in mat:
-        stroka = Label(root, bg='#BBD9B2', width= 108, height= 5)
-        stroka.place(x=20, y= y_posihion)
-        text_type = Label(stroka, text=f'{i.type}', bg = '#BBD9B2', font=('Gabriolla', 12))
-        text_type.place(x=5, y= 5)
-        text_name = Label(stroka, text=f'| {i.name}' , bg = '#BBD9B2', font=('Gabriolla', 12))
-        text_name.place(x= 70, y=5)
-        text_min_core = Label(stroka, text=f'Минимальное количество: {i.min_core}',
-                            bg= '#BBD9B2', font=('Gabriolla', 9))
+        stroka = Label(root, bg='#BBD9B2', width=108, height=5)
+        stroka.place(x=20, y=y_posihion)
+        text_type = Label(stroka, text=f'{i.type}', bg='#BBD9B2',
+                          font=('Gabriolla', 12))
+        text_type.place(x=5, y=5)
+
+        text_name = Label(stroka, text=f'| {i.name}', bg='#BBD9B2',
+                          font=('Gabriolla', 12))
+        text_name.place(x=70, y=5)
+        text_min_core = Label(stroka,
+                              text=f'Минимальное количество: {i.min_core}',
+                              bg='#BBD9B2', font=('Gabriolla', 9))
         text_min_core.place(x=5, y=25)
 
-        text_core = Label(stroka, text=f'Количество на складе: {i.core}' ,
-                        bg= '#BBD9B2',  font=('Gabriolla', 9))
+        text_core = Label(stroka, text=f'Количество на складе: {i.core}',
+                          bg='#BBD9B2',  font=('Gabriolla', 9))
         text_core.place(x=5, y=40)
         text_price = Label(stroka,
-                            text= f'Цена: {i.edinisa} /единица измерения | {i.core_ypakovka} ',
-                        bg= '#BBD9B2',  font=('Gabriolla', 9))
+                           text=f'Цена: {i.edinisa} /единица измерения | \
+                           {i.core_ypakovka}',
+                           bg='#BBD9B2',  font=('Gabriolla', 9))
         text_price.place(x=5, y=55)
         text_tr_core = Label(stroka, text=f'Требуемое количество: {i.tr_core}',
-                            bg = '#BBD9B2', font=('Gabriolla', 12))
-        text_tr_core.place(x=500, y= 5)
+                             bg='#BBD9B2', font=('Gabriolla', 12))
+        text_tr_core.place(x=500, y=5)
 
         y_posihion += 90
 
@@ -61,33 +74,34 @@ def first_window(event=None):
         root_two.resizable(False, False)
         root_two.configure(bg='#FFFFFF')
 
-        text_type = Label(root_two, text=  'Тип' ,
-                          bg='#FFFFFF' , font= ('Gabriolla' , 9))
+        text_type = Label(root_two, text='Тип',
+                          bg='#FFFFFF', font=('Gabriolla', 9))
         text_type.place(x=10, y=10)
 
         type_pol = Entry(root_two)
-        type_pol.place(x= 200, y=10)
-        
-        text_name =Label(root_two, text=  'Наименование материала' ,
-                          bg='#FFFFFF' , font= ('Gabriolla' , 9))
-        text_name.place(x= 10, y=35)
+        type_pol.place(x=200, y=10)
+
+        text_name = Label(root_two, text='Наименование материала',
+                          bg='#FFFFFF', font=('Gabriolla', 9))
+        text_name.place(x=10, y=35)
 
         name_pol = Entry(root_two)
         name_pol.place(x=200, y=35)
 
-
         root_two.mainloop()
 
-        
-    button_grid = Button(root, width= 15, text= 'Создать', bg= '#2D6033' , fg= 'white',
-                       font=('Gabriolla', 9), command= funciion_button)
+    button_grid = Button(root, width=15, text='Создать', bg='#2D6033',
+                         fg='white',
+                         font=('Gabriolla', 9), command=funciion_button)
     button_grid.place(x=340, y=500)
 
+
 def two_window(event=None):
+    clear_window()
     '''Функционал окна '''
 
-    shapla = Label(root, text='Второе окно', bg='#2D6033', fg ='white',
-                font=('Gabriolla', 20), width=50, height=2)
+    shapla = Label(root, text='Второе окно', bg='#2D6033', fg='white',
+                   font=('Gabriolla', 20), width=50, height=2)
     shapla.place(x=0, y=15)
 
     image = PhotoImage(file='icon.png')
@@ -105,41 +119,47 @@ def two_window(event=None):
     y_posihion = 100
 
     for i in mat:
-        stroka = Label(root, bg='#BBD9B2', width= 108, height= 5)
-        stroka.place(x=20, y= y_posihion)
-        text_type = Label(stroka, text=f'{i.type}', bg = '#BBD9B2', font=('Gabriolla', 12))
-        text_type.place(x=5, y= 5)
-        text_name = Label(stroka, text=f'| {i.name}' , bg = '#BBD9B2', font=('Gabriolla', 12))
-        text_name.place(x= 70, y=5)
-        text_min_core = Label(stroka, text=f'Минимальное количество: {i.min_core}',
-                            bg= '#BBD9B2', font=('Gabriolla', 9))
+        stroka = Label(root, bg='#BBD9B2', width=108, height=5)
+        stroka.place(x=20, y=y_posihion)
+        text_type = Label(stroka, text=f'{i.type}', bg='#BBD9B2',
+                          font=('Gabriolla', 12))
+        text_type.place(x=5, y=5)
+        text_name = Label(stroka, text=f'| {i.name}', bg='#BBD9B2',
+                          font=('Gabriolla', 12))
+        text_name.place(x=70, y=5)
+        text_min_core = Label(stroka,
+                              text=f'Минимальное количество: {i.min_core}',
+                              bg='#BBD9B2', font=('Gabriolla', 9))
         text_min_core.place(x=5, y=25)
 
-        text_core = Label(stroka, text=f'Количество на складе: {i.core}' ,
-                        bg= '#BBD9B2',  font=('Gabriolla', 9))
+        text_core = Label(stroka, text=f'Количество на складе: {i.core}',
+                          bg='#BBD9B2',  font=('Gabriolla', 9))
         text_core.place(x=5, y=40)
         text_price = Label(stroka,
-                            text= f'Цена: {i.edinisa} /единица измерения | {i.core_ypakovka} ',
-                        bg= '#BBD9B2',  font=('Gabriolla', 9))
+                           text=f'Цена: {i.edinisa} /единица измерения | \
+                           {i.core_ypakovka} ',
+                           bg='#BBD9B2',  font=('Gabriolla', 9))
         text_price.place(x=5, y=55)
         text_tr_core = Label(stroka, text=f'Требуемое количество: {i.tr_core}',
-                            bg = '#BBD9B2', font=('Gabriolla', 12))
-        text_tr_core.place(x=500, y= 5)
+                             bg='#BBD9B2', font=('Gabriolla', 12))
+        text_tr_core.place(x=500, y=5)
 
         y_posihion += 90
 
     messagebox.showerror('ОШИБКА! ', 'Данные не обновились')
     messagebox.showwarning('ВНИМАНИЕ! ', 'Ошибка соединения ')
-    messagebox.showinfo('Успешно', 'Соединение восстановлено, данные обновлены')
+    messagebox.showinfo('Успешно',
+                        'Соединение восстановлено, данные обновлены')
 
-button_back = Label(root, text='Назад', bg = "#2D6033",
-                     fg= 'white', width=15,
-                     font=('Gabriolla', 9))
+
+button_back = Label(root, text='Назад', bg="#2D6033",
+                    fg='white', width=15,
+                    font=('Gabriolla', 9))
 button_back.place(x=220, y=550)
 button_back.bind('<Button-1>', lambda e: first_window())
 
-button_forward = Label(root, text='Вперёд', bg = "#2D6033",
-                     fg= 'white', width=15,  font=('Gabriolla', 9))
+button_forward = Label(root, text='Вперёд', bg="#2D6033",
+                       fg='white', width=15,  font=('Gabriolla', 9))
 button_forward.place(x=450, y=550)
 button_forward.bind('<Button-1>', lambda e: two_window())
 first_window()
